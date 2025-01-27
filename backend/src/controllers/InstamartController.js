5abfeb2490d0ddf9cfab06071e0dd6bed31aa7dc
hari shanker sharma <67834698+triggered-exe@users.noreply.github.com>
2025-01-27 10:00:32 +0530

    revert

--- backend/src/controllers/InstamartController.js
+++ backend/src/controllers/InstamartController.js
@@ -274,8 +274,6 @@ export const trackProductPrices = async () => {
 
       const droppedProducts = await InstamartProduct.find({
         priceDroppedAt: { $gte: new Date(Date.now() - HALF_HOUR) }
-        discount: { $gte: 40 },
-        priceDropNotificationSent: { $exists: true, $eq: false }
       }).sort({ discount: -1 });
 
       // Send both email and Telegram notifications
