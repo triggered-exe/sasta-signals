482c99a40468a1c68e691911ac6471a498f846ad
triggered-exe <harishankersharma648@gmail.com>
2025-05-25 19:42:53 +0530

    hiding the hydration warning

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
